59fd34b65af0b00e396eaa81708ab8dffaeb792f
NataliaNarkiewicz <natalianarkiewicz7@gmail.com>
2025-06-13 11:39:40 +0200

    Create narkiewicz_zewnetrzny.js

---
+++ narkiewicz_zewnetrzny.js
@@ -0,0 +1,9 @@
+
+document.write(
+  "ostatnia modyfikacja strony"
+    .fontcolor("#0000B0")   // kolor 11
+    .bold()
+    .strike()
+    .fontsize(5) + "<br>"
+);
+document.write(document.lastModified);
